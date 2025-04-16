874dde32c8bad6a561301f92c155a5efe377bab1
YuranBen <yuran.benbraham.pro@gmail.com>
2025-04-16 15:18:40 +0200

    Update variables.c

---
+++ TP1/src/variables.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+
+    char c = "a";
+    unsigned char c1 = 1;
+    int i = 2;
+    unsigned int i1 = 3;
+    short s = 4;
+    unsigned short s1 = 5;
+    long int l = 6;
+    unsigned long int l1 = 7;
+    long long int li = 8;
+    unsigned long long int li1 = 9;
+
+    float f = 10.1;
+    double d = 11.2;
+    long double d1 = 12.3;
+
+  
+    printf("char : %c et unsigned char : %u\n", c,c1);
+    printf("int : %d et unsigned int : %u\n", i,i1);
+    printf("short : %hd et unsigned short : %u\n", s,s1);
+    printf("long : %ld et unsigned long : %u\n", li,l1);
+    printf("long long : %ld et unsigned long : %u\n", li,li1);
+    printf("float : %f\n", f);
+    printf("double : %f\n", d);
+    printf("long double : %f\n", d1);
+}
+  
