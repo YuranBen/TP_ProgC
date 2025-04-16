9cb64aa48623662ef4c78de9b1856f3fa3f3e6de
YuranBen <yuran.benbraham.pro@gmail.com>
2025-04-16 14:55:11 +0200

    Update sizeof_types.c

---
+++ TP1/src/sizeof_types.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    cout << "sizeof(unsigned char) = " << sizeof(unsigned char) << endl;
+    cout << "sizeof(signed char) = " << sizeof(signed char) << endl;
+    cout << "sizeof(unsigned short) = " << sizeof(short) << endl;
+    cout << "sizeof(short) = " << sizeof(short int) << endl;
+    cout << "sizeof(int) = " << sizeof(int) << endl;
+    cout << "sizeof(long int) = " << sizeof(long int) << endl;
+    cout << "sizeof(long long int) = " << sizeof(long long int) << endl;
+    cout << "sizeof(float) = " << sizeof(float) << endl;
+    cout << "sizeof(double) = " << sizeof(double) << endl;
+    cout << "sizeof(long double) = " << sizeof(long double) << endl;
+}
+  
