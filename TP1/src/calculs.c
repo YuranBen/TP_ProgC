342475afedf8f12b9548b611920ad756a928891b
YuranBen <yuran.benbraham.pro@gmail.com>
2025-04-16 17:00:17 +0200

    Update calculs.c

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,49 @@
+#include <stdlib.h>
+#include <stdio.h>
 
+int main()
+{
+  int num1,num2;
+  char op;
+  float res;
+
+  printf("entrez le premier chiffre : \n");
+  scanf("%d",&num1);
+
+  printf("entrez le second chiffre : \n");
+  scanf("%d",&num2);
+
+  printf("entrez l'opérateur : \n");
+  scanf("%c",&op);
+
+  switch(op)
+  {
+    case '+':
+      res = num1 + num2;
+      break;
+    case '-':
+      res = num1 - num2;
+      break;
+    case '*':
+      res = num1 * num2;
+      break;
+    case '/':
+      res = num1 / num2;
+      break;
+    case '%':
+      res = num1 % num2;
+      break;
+    case '&':
+      res = num1 & num2;
+      break;
+    case '|':
+      res = num1 | num2;
+      break;
+    case '~':
+      res = num1 ~ num2;
+      break;
+    default:
+      printf("Mauvais opérateur \n");
+      break;
+    }
+}
