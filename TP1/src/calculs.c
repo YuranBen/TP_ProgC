8cf89ac2b4b05016a463be0c5fe71fccd068ecae
YuranBen <yuran.benbraham.pro@gmail.com>
2025-04-16 17:11:52 +0200

    Update calculs.c

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -7,14 +7,15 @@ int main()
   char op;
   float res;
 
+  printf("entrez l'opérateur : \n");
+  scanf("%c",&op);
+
   printf("entrez le premier chiffre : \n");
   scanf("%d",&num1);
 
   printf("entrez le second chiffre : \n");
   scanf("%d",&num2);
 
-  printf("entrez l'opérateur : \n");
-  scanf("%c",&op);
 
   switch(op)
   {
@@ -40,7 +41,7 @@ int main()
       res = num1 | num2;
       break;
     case '~':
-     
+      res = ~num2;
       break;
     default:
       printf("Mauvais opérateur \n");
