23a6ee6b89d2974ef1295c6257a856433e774f4c
YuranBen <yuran.benbraham.pro@gmail.com>
2025-04-16 15:50:17 +0200

    Update boucles.c

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -5,9 +5,9 @@ int main()
   int i,j;
   printf("Combien d'étoiles ?\n"); 
   scanf ("%d",i);
-  for ((i=0 ; i<$value ; i++))
+  for (i=0 ; i<$value ; i++)
     {
-      for ((j=0 ; j<i ; j++))
+      for (j=0 ; j<i ; j++)
         {
           printf("*");
         }
