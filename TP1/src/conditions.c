b838c610976c60d4994011dc7ede997b06746e7c
YuranBen <yuran.benbraham.pro@gmail.com>
2025-04-16 16:36:14 +0200

    Update conditions.c

---
+++ TP1/src/conditions.c
@@ -0,0 +1,27 @@
+#include <std.io>
+#include <std.lib>
+
+int main()
+{
+  int i;
+  int somme;
+
+  for(i=0;i<1000;i++)
+  {
+    if(i%5 == 0 || i%7 == 0)
+    {
+      somme=somme+i;
+    }
+    else {
+        if(i%11 == 0)
+        {
+          continue;
+        }
+    }
+    if(somme > 5000)
+    {
+      break;
+    }
+  }
+  printf("resultat : %d",somme);
+}
