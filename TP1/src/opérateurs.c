7ca9443b41746d8df269f4f21e7d52062195e388
YuranBen <yuran.benbraham.pro@gmail.com>
2025-04-16 15:40:33 +0200

    Update opérateurs.c

---
+++ TP1/src/opérateurs.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+  int a = 16;
+  int b = 3;
+
+  int add = a + b;
+  int sus = a - b;
+  int mul = a*b;
+  int div = a/b;
+  int res = a%b;
+  bool comp = a == b;
+  bool comp2 = a > b;
+
+  printf("%d, %d, %d, %d, %d",add,sus,mul,div,res);
+
+}
