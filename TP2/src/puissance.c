fb0059d68ed1ccce04b16c6af2c3adbfd331b059
YuranBen <yuran.benbraham.pro@gmail.com>
2025-04-30 14:24:21 +0200

    Update puissance.c

---
+++ TP2/src/puissance.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+  int a = 2;
+  int b = 4;
+  int pui;
+  int i;
+
+  for(i=0;i<b+1;i++)
+  {
+    pui=a*b;
+  }
+
+  printf("resultat = %d",&pui);
+
+  return EXIT_SUCCESS;
+}
