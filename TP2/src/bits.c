d9340bdc23b8dfc56f4bf45e17d98f26a5a5ccb8
YuranBen <yuran.benbraham.pro@gmail.com>
2025-04-30 14:52:33 +0200

    Update bits.c

---
+++ TP2/src/bits.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+  int d = 8;
+  int d4 = d << 4;
+  int d20 = << 20;
+
+  if(d4 && d20 == 1)
+  {
+    printf("1");
+  }
+  else
+  {
+    printf("0");
+  }
+}
